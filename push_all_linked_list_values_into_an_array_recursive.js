1adce35082298737582b6b9f564322819c899c5e
Dennis Kozevnikoff <33944866+denniskozevnikoff@users.noreply.github.com>
2023-01-29 15:31:31 -0500

    Create push_all_linked_list_values_into_an_array_recursive.js
    simple script to illustrate how to traverse a linked list, and get all values in one variable

---
+++ push_all_linked_list_values_into_an_array_recursive.js
@@ -0,0 +1,32 @@
+class Node {
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+const linkedListValues = (head) => {
+    
+    const values = [];
+    fillValues(head, values);
+    return values;
+    
+};
+
+fillValues = (head, values) => {
+    if(head == null) return;
+    values.push(head.val);
+    fillValues(head.next, values);
+    
+};
+
+console.log(linkedListValues(a));
