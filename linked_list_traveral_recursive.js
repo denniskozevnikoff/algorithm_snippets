27d1be53737320dce20d779f67786debbef46f11
Dennis Kozevnikoff <33944866+denniskozevnikoff@users.noreply.github.com>
2023-01-29 14:53:19 -0500

    Create linked_list_traveral_recursive.js
    recursive way to traverse every node in a singly linked list

---
+++ linked_list_traveral_recursive.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+//   a -> b -> c -> d -> null
+// curr
+
+
+const printLinkedList = (head) => {
+    let current = head;
+    if(current != null) {
+        console.log(current.val); 
+        printLinkedList(current.next);
+    }
+       
+        
+     
+    
+};
+
+printLinkedList(a);
